17a52763482e39d9db73c7188ec9f6f0fa565864
Unknown <catreana@gmail.com>
2020-03-25 16:47:09 -0700

    Completed WorkPackageReportSnapshot

---
+++ COMP4911WebAPI/Models/WorkPackageReportSnapshot.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace COMP4911WebAPI.Models
+{
+    public class WorkPackageReportSnapshot
+    {
+        //A snapshot of the work package data at the time the report is run 
+        //the purpose is to allow historical reports to be run again even if details on work
+        //packages change later 
+
+        //note: this is a composite key of workpackageid and projectreportid
+        [Key]
+        public int WorkPackageId { get; set; }
+        [Key]
+        public int ProjectReportId { get; set; }
+        public string WorkPackageCode { get; set; }
+        public string WorkPackageTitle { get; set; }
+        public double WorkPackageResponsibleEngineerBudget { get; set; } //wpReBudget
+        public double WorkPackageActualSpends { get; set; }
+        public double WorkPackageResponsibleEngineerEstimateAtCompletion { get; set; }
+        public double WorkPackageProjectManagerEstimateAtCompletion { get; set; }
+        public double LabourGradeWage { get; set; }
+        public WorkPackage HighWorkPackage { get; set; }
+        public WorkPackage LowWorkPackage { get; set; }
+    }
